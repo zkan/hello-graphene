e6765dc72b69cbce4c97914d7a0a733c65a1e629
Kan Ouivirach <kan@prontomarketing.com>
2016-11-02 20:45:12 +0700

    Add a bit more complex example

--- example.py
+++ example.py
@@ -1,6 +1,15 @@
 import graphene
 
 
+class GeoInput(graphene.InputObjectType):
+    lat = graphene.Float(required=True)
+    lng = graphene.Float(required=True)
+
+
+class Address(graphene.ObjectType):
+    latlng = graphene.String()
+
+
 class Account(graphene.ObjectType):
     id = graphene.ID()
     name = graphene.String()
@@ -9,12 +18,17 @@ class Account(graphene.ObjectType):
 class Query(graphene.ObjectType):
     hello = graphene.String(description='A typical hello world')
     account = graphene.Field(Account)
+    address = graphene.Field(Address, geo=GeoInput())
 
     def resolve_hello(self, args, context, info):
         return 'World'
 
     def resolve_account(self, args, context, info):
         return Account(id=1, name='Pronto')
+
+    def resolve_address(self, args, context, info):
+        geo = args.get('geo')
+        return Address(latlng="({},{})".format(geo.get('lat'), geo.get('lng')))
 
 
 schema = graphene.Schema(query=Query)
@@ -25,6 +39,9 @@ query = '''
         account {
             name
         }
+        address(geo: {lat: 32.2, lng: 12}) {
+            latlng
+        }
     }
 '''
 
@@ -32,4 +49,5 @@ result = schema.execute(query)
 
 print(result.data)
 print(result.data['hello'])
-print(result.data['account'])
+print(result.data['account']['name'])
+print(result.data['address']['latlng'])
